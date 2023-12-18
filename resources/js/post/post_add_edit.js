65ee847c23cd48f3b354841faad36baf5d2a996b
anhlp1212 <luongphuonganh30@gmail.com>
2023-12-18 23:24:55 +0700

    feature/BogPage-Laravel-DesignInterface: Modify css Page Admin

--- resources/js/post/post_add_edit.js
+++ resources/js/post/post_add_edit.js
@@ -2,9 +2,11 @@ function loadFile(event) {
     var image = document.querySelector('#box_image_post');
     if (event.target.files[0] != null) {
         image.src = URL.createObjectURL(event.target.files[0]);
+        image.style.display = "block";
         return true;
     } else {
         image.src = "";
+        image.style.display = "none";
         return false;
     }
 }
--- resources/views/admin/posts/edit_post.blade.php
+++ resources/views/admin/posts/edit_post.blade.php
@@ -10,91 +10,74 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-body px-0 pb-2">
-                        <div class="Styles__InfoPage">
-                            <div class="info-row">
-                                <div class="info-left-addsp">
-                                    <div class="Style__AccountInfo">
-                                        @if (session('warning'))
-                                            <div class="alert alert-warning"> {{ session('warning') }} </div>
-                                        @endif
-                                        @if (Route::has('post.edit_post'))
-                                            <form class="form-edit-sp" id="form-edit-sp" method="post"
-                                                action="{{ route('post.edit_post') }}" enctype="multipart/form-data">
-                                                @csrf
+                        @if (session('warning'))
+                            <div class="alert alert-warning"> {{ session('warning') }} </div>
+                        @endif
+                        @if (Route::has('post.edit_post'))
+                            <form class="form-edit-sp" id="form-edit-sp" method="post" action="{{ route('post.edit_post') }}"
+                                enctype="multipart/form-data">
+                                @csrf
 
-                                                <div class="my-form-control">
-                                                    <label class="input-label">Post</label>
-                                                    <div>
-                                                        <div class="Style__StyleInput">
-                                                            <input type="hidden" name="id"
-                                                                value="{{ $post->id }}">
-                                                            <input class="input_fullName" id="tensp" type="text"
-                                                                name="title" maxlength="128" placeholder="Add Title Post"
-                                                                value=" {{ $post->title }}">
-                                                            <div>
-                                                                <small class="small">
-                                                                    @error('title')
-                                                                        <div class="error">
-                                                                            {{ $message ?? 'Error' }}
-                                                                        </div>
-                                                                    @enderror
-                                                                </small>
-                                                            </div>
+                                <div class="my-form-control">
+                                    <label class="input-label">Post</label>
+                                    <div>
+                                        <div class="Style__StyleInput">
+                                            <input type="hidden" name="id" value="{{ $post->id }}">
+                                            <input class="input_fullName" id="tensp" type="text" name="title"
+                                                maxlength="128" placeholder="Add Title Post" value=" {{ $post->title }}">
+                                            <div class="mt-2">
+                                                <small class="small">
+                                                    @error('title')
+                                                        <div class="error">
+                                                            {{ $message ?? 'Error' }}
                                                         </div>
-                                                    </div>
-                                                </div>
-
-                                                <div class="my-form-control">
-                                                    <label class="input-label">Description</label>
-                                                    <div>
-                                                        <div class="Style__StyleInput">
-                                                            <!-- Insert the blade containing the TinyMCE placeholder HTML element -->
-                                                            <textarea id="myeditorinstance" placeholder="Not Empty!" name="description"> {{ $post->description }}</textarea>
-                                                            <div>
-                                                                <small class="small">
-                                                                    @error('description')
-                                                                        <div class="error">
-                                                                            {{ $message ?? 'Error' }}
-                                                                        </div>
-                                                                    @enderror
-                                                                </small>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-
-                                                <div class="my-form-control">
-                                                    <label class="input-label">Image</label>
-                                                    <div>
-                                                        <img src="{{ asset($post->image) }}" alt="Post Image"
-                                                            class="box_image_post" id="box_image_post" />
-                                                        <input class="input_fullName" type="file" id="image_post"
-                                                            name="image" accept="image/png, image/jpeg"
-                                                            value="{{ asset($post->image) }}" />
-                                                        <div>
-                                                            <small class="small">
-                                                                @error('image')
-                                                                    <div class="error">
-                                                                        {{ $message ?? 'Error' }}</div>
-                                                                @enderror
-                                                            </small>
-                                                        </div>
-                                                    </div>
-                                                </div>
-
-                                                <br />
-                                                <div class="my-form-control">
-                                                    <label class="input-label">&nbsp;</label>
-                                                    <button type="submit"
-                                                        class="Style__StyleBtnSubmit btn-submit">Save</button>
-                                                </div>
-                                            </form>
-                                        @endif
+                                                    @enderror
+                                                </small>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="info-vertical"></div>
-                            </div>
-                        </div>
+
+                                <div class="my-form-control">
+                                    <label class="input-label">Description</label>
+                                    <div>
+                                        <div class="Style__StyleInput">
+                                            <!-- Insert the blade containing the TinyMCE placeholder HTML element -->
+                                            <textarea id="myeditorinstance" placeholder="Not Empty!" name="description"> {{ $post->description }}</textarea>
+                                            <div class="mt-2">
+                                                <small class="small">
+                                                    @error('description')
+                                                        <div class="error">
+                                                            {{ $message ?? 'Error' }}
+                                                        </div>
+                                                    @enderror
+                                                </small>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="my-form-control">
+                                    <img src="{{ asset($post->image) }}" alt="Post Image" class="img-fluid box_image_post"
+                                        id="box_image_post" />
+                                    <input class="mt-2" type="file" id="image_post" name="image"
+                                        accept="image/png, image/jpeg, image/jpg" value="{{ asset($post->image) }}" />
+                                    <div class="mt-2">
+                                        <small class="small">
+                                            @error('image')
+                                                <div class="error">
+                                                    {{ $message ?? 'Error' }}</div>
+                                            @enderror
+                                        </small>
+                                    </div>
+                                </div>
+
+                                <br />
+                                <div class="my-form-control">
+                                    <button type="submit" class="btn btn-submit bg-gradient-primary">Save</button>
+                                </div>
+                            </form>
+                        @endif
                     </div>
                 </div>
             </div>
